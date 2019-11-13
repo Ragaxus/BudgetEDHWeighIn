d73a4a4957e0b987a4deebb45d10ef804691d02a
sgold <ragaxus@gmail.com>
2019-11-13 09:56:48 -0600

    Correctly handle foil-only cards; add err report

--- budgetEDHWeighIn.js
+++ budgetEDHWeighIn.js
@@ -18,18 +18,39 @@ function showPriceResults() {
         //Make list of cards by lowest price (excluding sets that we don't want to consider)
         var resList = response.data;
         var priceIdx=0;
-        while (!(price>0)) {
-            var printing = resList[priceIdx];
-            if ((printing.prices.usd)&&(printing.set_type != "memorabilia")) {
-                price=Number(printing.prices.usd);
-                set=printing.set_name;
+        var priceToUse=0;
+        if (resList) {
+            while ((!(price>0))&&(priceIdx<resList.length)) {
+                var printing = resList[priceIdx];
+                priceToUse=getPriceToUse(printing)
+                if ((priceToUse)&&(printing.set_type != "memorabilia")) {
+                    price=Number(priceToUse);
+                    set=printing.set_name;
+                }
+                else priceIdx++;
             }
-            else priceIdx++;
         }
         //Output total
-        document.getElementById("cardPriceList").innerHTML += "<p>"+name+" -- $"+price+" ("+set+") </p>";
-        totalPrice += price;
-        document.getElementById("totalPrice").innerText = totalPrice.toFixed(2);
+        if (price>0) {
+            document.getElementById("cardPriceList").innerHTML += "<p>"+name+" -- $"+price+" ("+set+") </p>";
+            totalPrice += price;
+            document.getElementById("totalPrice").innerText = totalPrice.toFixed(2);
+        }
+        else {
+            var errListHeader=document.getElementById("errListHeader");
+            if(typeof(errListHeader) == 'undefined' || errListHeader == null) { 
+                document.getElementById("errList").innerHTML += "<p id='errListHeader'>"+"Could not find prices for the following cards: "+"</p>";
+            }
+            document.getElementById("errList").innerHTML += "<p>"+name+"</p>";
+        }
         });        
     });
+}
+
+function getPriceToUse(printing) {
+    var priceReg = Number(printing.prices.usd)
+    var priceFoil = Number(printing.prices.usd_foil)
+    if (!priceReg) return priceFoil;
+    if (!priceFoil) return priceReg;
+    return Math.min(priceFoil,priceReg);
 }

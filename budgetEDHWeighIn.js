801f2e466bb816d796054ef411745091b3867091
Ragaxus <ragaxus@gmail.com>
2019-11-19 21:07:31 -0600

    Add toggle to sort by price arr/dsc; >30 err msg

--- budgetEDHWeighIn.js
+++ budgetEDHWeighIn.js
@@ -10,6 +10,12 @@ function showPriceResults() {
     //Call /card/collection
     var searchUrl = new URL("https://api.scryfall.com/cards/search");
     var params = {"unique":"prints","order":"usd","dir":"asc"};
+    var cardResultArr = [];
+    var hasAlerted = false;
+
+    var limit=30.0;
+    var errMsg = "you fool. you absolute baffoon. You think you can challenge me in my own realm? you think you can rebel against the agreed upon deck price limit, which is $"+limit+"? you dare come into my house and upturn my dining chairs and spill coffee grounds in my Keurig? you thought you were safe in your chain mail armor behind that screen of yours. I will take these laminate wood floor boards and destroy you. i didnt want war. but i didnt start it"
+
     namePromiseList = nameList.map(name => { return new Promise( function(res,rej) {
         var price=0;
         var set;
@@ -31,11 +37,12 @@ function showPriceResults() {
                 else priceIdx++;
             }
         }
-        //Output total
+        //Update running total and cache result
         if (price>0) {
-            document.getElementById("cardPriceList").innerHTML += "<p>"+name+" -- $"+price+" ("+set+") </p>";
             totalPrice += price;
+            if ((totalPrice>limit) && !hasAlerted) {hasAlerted=true; alert(errMsg)};
             document.getElementById("totalPrice").innerText = totalPrice.toFixed(2);
+            cardResultArr.push({"name": name, "price": price, "set":set});
         }
         else {
             var errListHeader=document.getElementById("errListHeader");
@@ -49,6 +56,20 @@ function showPriceResults() {
         })});        
     });
     Promise.all(namePromiseList).then(function (vals) {
+        var sortMethod;
+        if (document.getElementById("sortExpensiveFirst").checked) {
+            sortMethod = function(a,b) { return (a.price < b.price) ? 1 : -1};
+        }
+        else {
+            sortMethod = function(a,b) { return (a.price > b.price) ? 1 : -1};
+        }
+        cardResultArr.sort(sortMethod);
+        cardResultArr.map(result => {
+            var parg = document.createElement("p");
+            parg.appendChild(document.createTextNode(result.name+" -- $"+result.price+" ("+result.set+")"));
+            document.getElementById("cardPriceList").appendChild(parg);
+        });
+
         document.getElementById("totalPrice").innerText += ", as of "+new Date().toLocaleString();
         document.getElementById("saveResults").disabled = false;
     });
